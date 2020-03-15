3ce4422e09753cb7c5307771daf4822df43e6664
Kelly Gothard <kellygothard@gmail.com>
2020-03-15 10:48:42 -0400

    columns distributions

--- src/explore_files16hz.py
+++ src/explore_files16hz.py
@@ -3,18 +3,16 @@ import glob
 import matplotlib.pyplot as plt
 
 path = '../files16hz/*.csv'
+image_path = '../images/dists/'
 paths = glob.glob(path)
 
-print(paths)
-
-for file_path in paths:
-    df = pd.read_csv(file_path,header=None)
-    break
-
-print(df.head())
-print(df.columns)
+df = pd.read_csv(paths[0], header=None)
+for file_path in paths[1:]:
+    more_df = pd.read_csv(file_path,header=None)
+    df = df.append(more_df)
 
 for col in df.columns:
-    plt.hist(df[col], bins = len(df)//5)
+    plt.hist(df[col],bins=100)
     plt.title(str(col))
-    plt.show()
+    plt.savefig(image_path+'col_'+str(col)+'_dist.png')
+    plt.close()
